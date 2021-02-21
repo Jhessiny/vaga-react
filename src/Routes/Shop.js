3e9441d6ace71ee5e432fe167559bb51976e2818
jhessiny <jhessinymattos@gmail.com>
2021-02-21 16:46:06 -0300

    main background

--- src/Routes/Shop.js
+++ src/Routes/Shop.js
@@ -2,7 +2,7 @@ import Product from "../components/Product/ProductItem";
 
 const Shop = ({ products, addToCart }) => {
   return (
-    <div className="products-list container mt-5 d-flex flex-wrap justify-content-between">
+    <div className="products-list container pt-5 d-flex flex-wrap justify-content-between">
       {products.map((product) => (
         <Product
           product={product}
